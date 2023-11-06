ee16f077dd27569e8c6c0e5256e5bf74abde0a46
Prajwal Kailas <prajwal967@gmail.com>
2023-11-06 14:38:19 -0500

    rename folder

---
+++ src/training/parameters/instruct/code_instruct/__init__.py
@@ -0,0 +1,3 @@
+from .code_instruct_parameters import get_code_instruct_arguments
+from .encounter_file_parameters import get_encounter_file_arguments
+__all__ = ["get_code_instruct_arguments", "get_encounter_file_arguments"]
---
+++ src/training/parameters/instruct/code_instruct/code_instruct_parameters.py
@@ -0,0 +1,43 @@
+"""Parameters for train prompts"""
+import argparse
+
+
+def get_code_instruct_arguments():
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument(
+        "--billable-probability",
+        type=float,
+        default=0.0,
+        help="Probability to get the billable icd code as is"
+    )
+    parser.add_argument(
+        "--top-non-probability",
+        type=float,
+        default=1.0,
+        help="Probability to get the top most icd code in the hierarchy"
+    )
+    parser.add_argument(
+        "--mixed-non-probability",
+        type=float,
+        default=0.0,
+        help="Probability to get any code in the hierarchy"
+    )
+    parser.add_argument(
+        "--random-negative-probability",
+        type=float,
+        default=1.0,
+        help="Probability of sampling negative codes randomly"
+    )
+    parser.add_argument(
+        "--lock-range",
+        default=False,
+        action="store_true",
+        help="Whether to used fixed time ranges in the prompt"
+    )
+    parser.add_argument(
+        "--lowercase_icd_text",
+        default=False,
+        action="store_true",
+        help="Whether to lowercase icd text"
+    )
+    return parser
