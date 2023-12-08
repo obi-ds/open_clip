2569c6c09ee0ee160224a453b1f5d5f1181a00f3
Prajwal Kailas <prajwal967@gmail.com>
2023-12-07 20:07:01 -0500

    remove unused functions

--- src/training/instruct/utils.py
+++ src/training/instruct/utils.py
@@ -2,9 +2,7 @@
 import numpy as np
 from .codes.templates import (
     CodeStatusInstructionTemplate,
-    CodeStatusRangeInstructionTemplate,
     CodeStatusClassificationInstructions,
-    CodeStatusFixedRangeClassificationInstructions,
     CodeT2EInstructionTemplate,
     CodeT2EPredictionInstructions
 )
@@ -38,34 +36,6 @@ def get_code_status_classification_instructions(
         negative_answer_target=negative_answer_target
     )
 
-def get_code_status_range_classification_instructions(
-        task_definition: str,
-        future_input: str,
-        future_target: str,
-        past_input: str,
-        past_target: str,
-        positive_answer_target: str,
-        negative_answer_target: str,
-        lock_range: bool
-):
-    instruction_template_future = CodeStatusRangeInstructionTemplate(
-        inputs=future_input,
-        targets=future_target
-    )
-
-    instruction_template_past = CodeStatusRangeInstructionTemplate(
-        inputs=past_input,
-        targets=past_target
-    )
-
-    return CodeStatusFixedRangeClassificationInstructions(
-        task_definition=task_definition,
-        instruction_template_future=instruction_template_future,
-        instruction_template_past=instruction_template_past,
-        positive_answer_target=positive_answer_target,
-        negative_answer_target=negative_answer_target,
-        lock_range=lock_range
-    )
 
 def get_code_t2e_prediction_instructions(
         task_definition: str,
