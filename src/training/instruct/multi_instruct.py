eabe042c259dfb1c0501e504effa31823f323ed2
Prajwal Kailas <prajwal967@gmail.com>
2023-12-07 20:07:37 -0500

    use new instruct task code

--- src/training/instruct/multi_instruct.py
+++ src/training/instruct/multi_instruct.py
@@ -13,7 +13,7 @@ class MultiInstruct(object):
         self._code_status_classification_task = code_status_classification_task
         self._code_t2e_prediction_task = code_t2e_prediction_task
         self._icd_task_object = self.get_icd_task_object()
-        # TODO: Define task probabilities
+        # TODO: Define task probabilities and task in a better way
         self._tasks = self.get_tasks()
         self._task_probabilities = [0.5, 0.5]
 
@@ -41,13 +41,15 @@ class MultiInstruct(object):
             k_shot,
             sort_position,
     ):
-        return self._code_status_classification_task.process_sample(
+        instruction_samples, instructions = self._code_status_classification_task.get_task_instructions(
             encounter_history=encounter_history,
             all_positives=all_positives,
             positives=positives,
             k_shot=k_shot,
             sort_position=sort_position,
         )
+
+        return instructions
 
     def get_code_t2e_prediction_instructions(
             self,
@@ -57,13 +59,15 @@ class MultiInstruct(object):
             k_shot,
             sort_position,
     ):
-        return self._code_t2e_prediction_task.process_sample(
+        instruction_samples, instructions = self._code_t2e_prediction_task.get_task_instructions(
             encounter_history=encounter_history,
             all_positives=all_positives,
             positives=positives,
             k_shot=k_shot,
             sort_position=sort_position,
         )
+
+        return instructions
 
     def load_data_for_icd_task(self, sample, args):
 
@@ -88,17 +92,10 @@ class MultiInstruct(object):
         )
 
     def process_sample_from_args(self, sample, args):
-        """
-
-        Args:
-            sample:
-            args:
-
-        Returns:
-
-        """
         number_of_instructions = 5
-        k_shot_choices = [0, 1, 2]
+        k_shot_choices = [0]
+        sort_code_status_position = True
+        sort_code_t2e_position = True
 
         if self._icd_task_object is not None:
             encounter_history, all_positives, positives = self.load_data_for_icd_task(sample=sample, args=args)
@@ -114,8 +111,12 @@ class MultiInstruct(object):
                     all_positives=all_positives,
                     positives=positives,
                     k_shot=np.random.choice(k_shot_choices),
-                    sort_position=True
+                    sort_position=sort_code_status_position
                 )
+                # self.filter_encounter_history_by_code_position(
+                #     encounter_history=encounter_history,
+                #     position=instruction_samples[self._position_column][-1]
+                # )
 
             elif instruction_task == MultiTasks.CodeT2EPredictionTask:
                 instructions = self.get_code_t2e_prediction_instructions(
@@ -125,5 +126,9 @@ class MultiInstruct(object):
                     k_shot=np.random.choice(k_shot_choices),
                     sort_position=True
                 )
+            else:
+                raise ValueError('Invalid task')
             multi_task_instructions += instructions
+
+        return instructions
 
