b9e8292941df7f3d549cb7002f9b068ec906e33a
Prajwal Kailas <prajwal967@gmail.com>
2024-01-26 19:59:33 -0500

    remove code status and update docs

--- src/training/instruct/utils.py
+++ src/training/instruct/utils.py
@@ -1,56 +1,28 @@
 """Util functions"""
-import numpy as np
 from .codes.templates import (
-    CodeStatusInstructionTemplate,
-    CodeStatusClassificationInstructions,
-    CodeT2EInstructionTemplate,
-    CodeT2EPredictionInstructions
+    TimeBinCodeTemplate,
+    TimeBinPeriodTemplate,
+    CodeTrajectoryPredictionInstructions,
+    DescriptionInstructionTemplate
 )
 
-# ICD utils
+def get_code_trajectory_prediction_instructions() -> CodeTrajectoryPredictionInstructions:
+    """
+    Return the object can use the given data and instruction templates
+    and return an instruction string for the input and output
 
-def get_code_status_classification_instructions(
-        task_definition: str,
-        future_input: str,
-        future_target: str,
-        past_input: str,
-        past_target: str,
-        positive_answer_target: str,
-        negative_answer_target: str
-):
-    instruction_template_future = CodeStatusInstructionTemplate(
-        inputs=future_input,
-        targets=future_target
+    Returns:
+        (CodeTrajectoryPredictionInstructions): Object to build instruction strings (inputs and targets)
+    """
+    time_bin_period_instruction_template = TimeBinPeriodTemplate(inputs='\nDay {start_time} to {end_time}')
+    time_bin_code_instruction_template = TimeBinCodeTemplate(inputs='- {diagnosis}')
+    code_description_instruction_template = DescriptionInstructionTemplate(
+        inputs='* {diagnosis}',
+        targets='{description}'
     )
-
-    instruction_template_past = CodeStatusInstructionTemplate(
-        inputs=past_input,
-        targets=past_target
-    )
-
-    return CodeStatusClassificationInstructions(
-        task_definition=task_definition,
-        instruction_template_future=instruction_template_future,
-        instruction_template_past=instruction_template_past,
-        positive_answer_target=positive_answer_target,
-        negative_answer_target=negative_answer_target
-    )
-
-
-def get_code_t2e_prediction_instructions(
-        task_definition: str,
-        inputs: str,
-        targets: str,
-        negative_answer_target: float = np.inf,
-):
-    instruction_template = CodeT2EInstructionTemplate(
-        inputs=inputs,
-        targets=targets
-    )
-
-    return CodeT2EPredictionInstructions(
-        task_definition=task_definition,
-        instruction_template=instruction_template,
-        negative_answer_target=negative_answer_target,
+    return CodeTrajectoryPredictionInstructions(
+        time_bin_period_instruction_template=time_bin_period_instruction_template,
+        time_bin_code_instruction_template=time_bin_code_instruction_template,
+        code_description_instruction_template=code_description_instruction_template
     )
 
