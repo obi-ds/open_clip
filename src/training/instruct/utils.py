5f2398899a16cbe41702d929abb5f5fed619f049
Prajwal Kailas <prajwal967@gmail.com>
2024-01-31 12:46:41 -0500

    reforamt util fucntions

--- src/training/instruct/utils.py
+++ src/training/instruct/utils.py
@@ -14,7 +14,7 @@ def get_code_trajectory_prediction_instructions() -> CodeTrajectoryPredictionIns
     Returns:
         (CodeTrajectoryPredictionInstructions): Object to build instruction strings (inputs and targets)
     """
-    time_bin_period_instruction_template = TimeBinPeriodTemplate(inputs='\nDay {start_time} to {end_time}')
+    time_bin_period_instruction_template = TimeBinPeriodTemplate(inputs='\n* Day {start_time} to {end_time}')
     time_bin_code_instruction_template = TimeBinCodeTemplate(inputs='- {diagnosis}')
     code_description_instruction_template = DescriptionInstructionTemplate(
         inputs='* {diagnosis}',
