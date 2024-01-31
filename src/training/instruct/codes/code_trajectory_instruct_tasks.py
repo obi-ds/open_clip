2d40e46af3ab0e038359242b4a6a7bb6624d2a12
Prajwal Kailas <prajwal967@gmail.com>
2024-01-31 12:38:31 -0500

    add eval task

--- src/training/instruct/codes/code_trajectory_instruct_tasks.py
+++ src/training/instruct/codes/code_trajectory_instruct_tasks.py
@@ -16,7 +16,10 @@ class CodeTrajectoryPredictionTask(object):
     Make instruction tuning training data from diagnostic codes
     # TODO: Sampling whole bins v/s within bins,
     # TODO: Pre-train LM
-    # TODO: pre compute clusters, variable clusters sizes
+    # TODO: pre compute clusters, variable clusters sizes (specify cluster value in instruction string)
+    # TODO (cont) - model should know what's going on - especially the generative model
+    # TODO (cont) - mixed clustering - some part of the trajectory wide clusters, some parts with narrow clusters etc
+    # TODO (cont) - I think we can do this by applying clustering to different time periods with different cluster size
     # TODO: Generative pre-train  v/s instruction pre-train
     """
 
@@ -30,11 +33,11 @@ class CodeTrajectoryPredictionTask(object):
             patient_id_column: str = 'PatientID',
             code_column: str = 'ICD10CD',
             position_column: str = 'position',
-            instruction_label: str = 'instruction_label',
             bins_column: str = 'bins',
             bin_start_column: str = 'min',
             bin_end_column: str = 'max',
-            code_value_column: str = 'count'
+            code_value_column: str = 'count',
+            ignore_instruction_column: str = 'ignore_instruction'
     ):
         """
         Initialize variables
@@ -50,11 +53,13 @@ class CodeTrajectoryPredictionTask(object):
             code_column (str, defaults to `ICD10CD`): The column that contains the codes
             position_column (str, defaults to `positions`): The column where position values
             (e.g. time difference in days or log days) will be stored
-            instruction_label (str, defaults to `instruction_label`): Label assigned to the instructions - either
-            positive or negative
             bins_column (str, defaults to `bins`): Column that stores the assigned bin
             bin_start_column (str, defaults to `min`): Column that stores the start position of each bin
             bin_end_column (str, defaults to `max`): Column that stores the end position of each bin
+            code_value_column (str, defaults to `count`): The column that stores some value that represents
+            the code in a time bin
+            ignore_instruction_column (str, defaults to `ignore_instruction`): Column that stores whether
+            to ignore the instruction in that row
         """
         self._encounter_dataframe_process = encounter_dataframe_process
         self._dataframe_sampling = dataframe_sampling
@@ -63,14 +68,24 @@ class CodeTrajectoryPredictionTask(object):
         self._patient_id_column = patient_id_column
         self._code_column = code_column
         self._position_column = position_column
-        self._instruction_label = instruction_label
         self._time_bins = [time_bins] if not isinstance(time_bins, list) else  time_bins
         self._bins_column = bins_column
         self._bin_start_column = bin_start_column
         self._bin_end_column = bin_end_column
         self._code_value_column = code_value_column
+        self._ignore_instruction_column = ignore_instruction_column
 
     def process_sample(self, sample, args):
+        """
+        Process sample
+
+        Args:
+            sample:
+            args:
+
+        Returns:
+
+        """
 
         # Get processed encounter history
         encounter_history = self.get_encounter_history_for_task(
@@ -205,19 +220,34 @@ class CodeTrajectoryPredictionTask(object):
         )
         instruction_samples = pd.merge(sampled_encounter_history, bin_positions, how='left', on=self._bins_column)
         if shuffle:
-            return instruction_samples.groupby(self._bins_column).sample(frac=1).sort_values(by=self._bin_start_column)
+            instruction_samples = (
+                instruction_samples
+                .groupby(self._bins_column)
+                .sample(frac=1)
+                .sort_values(by=self._bin_start_column)
+            )
         else:
-            return instruction_samples.sort_values(by=self._position_column)
+            instruction_samples = instruction_samples.sort_values(by=self._position_column)
+
+        instruction_samples[self._ignore_instruction_column] = self.get_ignore_instructions(
+            instruction_samples=instruction_samples
+        )
+
+        return instruction_samples
 
     def convert_samples_to_instructions(
             self,
             instruction_samples: pd.DataFrame,
+            ignore_time_instruction: Optional[bool] = None
     ) -> List[Tuple[str, str]]:
         """
         Return the instruction input and targets
         for the sampled instruction code status classification task with time range
+
         Args:
             instruction_samples (pd.DataFrame): The samples that will be used as instructions
+            ignore_time_instruction(Optional[bool], defaults to `None`): Ignore the time period
+            instruction in output/labels
 
         Returns:
             (List[Tuple[str, str]]): A list that contains tuples which have the instruction input and target.
@@ -225,16 +255,26 @@ class CodeTrajectoryPredictionTask(object):
         """
 
         # description_instructions = self.get_code_description_instructions(instruction_samples=instruction_samples)
-        trajectory_instructions = self.get_code_trajectory_instructions(instruction_samples=instruction_samples)
+        trajectory_instructions = self.get_code_trajectory_instructions(
+            instruction_samples=instruction_samples,
+            ignore_time_instruction=ignore_time_instruction
+        )
 
         return trajectory_instructions
 
-    def get_code_trajectory_instructions(self, instruction_samples: pd.DataFrame):
+    def get_code_trajectory_instructions(
+            self,
+            instruction_samples: pd.DataFrame,
+            ignore_time_instruction: Optional[bool] = None
+    ):
         """
         Return the instruction input and targets
         that contain the time period and say counts of the codes in that time period
+
         Args:
             instruction_samples (pd.DataFrame): The samples that will be used as instructions
+            ignore_time_instruction(Optional[bool], defaults to `None`): Ignore the time period
+            instruction in output/labels
 
         Returns:
             (List[Tuple[str, str]]): A list that contains tuples which have the instruction input and target.
@@ -243,9 +283,9 @@ class CodeTrajectoryPredictionTask(object):
         instructions = list()
         start_flag = None
         for row in instruction_samples[
-            [self._code_column, self._code_value_column, self._bin_start_column, self._bin_end_column]
+            [self._code_column, self._code_value_column, self._bin_start_column, self._bin_end_column, self._ignore_instruction_column]
         ].itertuples(index=False):
-            code, code_value, bin_start, bin_end = row[0], row[1], row[2], row[3]
+            code, code_value, bin_start, bin_end, ignore_instruction = row[0], row[1], row[2], row[3], row[4]
 
             if start_flag is None or bin_start != start_flag:
                 start_flag = bin_start
@@ -253,18 +293,23 @@ class CodeTrajectoryPredictionTask(object):
                 time_instruct_string =  self._code_instructions.get_time_instruction(
                     start_time=int(bin_start), end_time=int(bin_end)
                 )
-                instructions.append(time_instruct_string)
+                instructions.append(
+                    time_instruct_string + (
+                        ignore_instruction if ignore_time_instruction is None else ignore_time_instruction,
+                    )
+                )
 
             code_instruct_string = self._code_instructions.get_code_instruction(
                 diagnosis=self._code_convert.transform_code(code=code), value=code_value
             )
-            instructions.append(code_instruct_string)
+            instructions.append(code_instruct_string + (ignore_instruction,))
         return instructions
 
     def get_code_description_instructions(self, instruction_samples: pd.DataFrame):
         """
         Return the instruction input and targets
         that contain the unique codes and their descriptions
+
         Args:
             instruction_samples (pd.DataFrame): The samples that will be used as instructions
 
@@ -284,6 +329,7 @@ class CodeTrajectoryPredictionTask(object):
         """
         Given the input dataframe - return the dataframe with each row
         put into specific bins
+
         Args:
             bin_value_column (pd.DataFrame): The column to use to calculate the bins
 
@@ -293,10 +339,25 @@ class CodeTrajectoryPredictionTask(object):
         time_bins_object = np.random.choice(self._time_bins)
         return time_bins_object.get_bins(bin_value_column)
 
+    def get_ignore_instructions(self, instruction_samples: pd.DataFrame) -> Union[List[bool], pd.Series]:
+        """
+        Return a dataframe of bools that represents whether to ignore instructions in the
+        output/labels
+
+        Args:
+           instruction_samples (pd.DataFrame): The samples that will be used as instructions
+
+        Returns:
+            ignore_instruction (Optional[Sequence[bool]]): Keep these instructions in the
+            input but don't use them as labels - ignore their output while training.
+        """
+        return [False] * len(instruction_samples)
+
     def get_random_encounters(self) -> pd.DataFrame:
         """
         Use this function to create a random dataframe populated
         with codes and positions
+
         Args:
 
         Returns:
@@ -312,3 +373,182 @@ class CodeTrajectoryPredictionTask(object):
             {self._code_column: codes, self._position_column: positions}
         )
 
+
+class CodeTrajectoryPredictionTaskEvaluation(CodeTrajectoryPredictionTask):
+    """
+    Make instruction tuning training data from diagnostic codes
+    """
+
+    def __init__(
+            self,
+            encounter_dataframe_process: EncounterDataframeProcess,
+            dataframe_sampling: GroupBySampling,
+            code_instructions: Any,
+            code_convert: Union[ICDConvert, PHEConvert],
+            time_bins: Union[AgglomerativeDataBins, Sequence[AgglomerativeDataBins]],
+            patient_id_column: str = 'PatientID',
+            code_column: str = 'ICD10CD',
+            position_column: str = 'position',
+            bins_column: str = 'bins',
+            bin_start_column: str = 'min',
+            bin_end_column: str = 'max',
+            code_value_column: str = 'count',
+            ignore_instruction_column: str = 'ignore_instruction'
+    ):
+        """
+        Initialize variables
+
+        Args:
+            encounter_dataframe_process (EncounterDataframeProcess): Dataframe processing object
+            dataframe_sampling (GroupBySampling): Dataframe sampling object
+            code_instructions (Any): A list of code instruction tasks. At any point one of these
+            tasks is randomly chosen and trained on
+            code_convert (Union[ICDConvert, PHEConvert]): The object to map codes to other codes in hierarchy and
+            their textual descriptions.
+            patient_id_column (str, defaults to `PatientID`): The column name for the patient id
+            code_column (str, defaults to `ICD10CD`): The column that contains the codes
+            position_column (str, defaults to `positions`): The column where position values
+            (e.g. time difference in days or log days) will be stored
+            bins_column (str, defaults to `bins`): Column that stores the assigned bin
+            bin_start_column (str, defaults to `min`): Column that stores the start position of each bin
+            bin_end_column (str, defaults to `max`): Column that stores the end position of each bin
+            code_value_column (str, defaults to `count`): The column that stores some value that represents
+            the code in a time bin
+            ignore_instruction_column (str, defaults to `ignore_instruction`): Column that stores whether
+            to ignore the instruction in that row
+        """
+        super().__init__(
+            encounter_dataframe_process=encounter_dataframe_process,
+            dataframe_sampling=dataframe_sampling,
+            code_instructions=code_instructions,
+            code_convert=code_convert,
+            time_bins=time_bins,
+            patient_id_column=patient_id_column,
+            code_column=code_column,
+            position_column=position_column,
+            bins_column=bins_column,
+            bin_start_column=bin_start_column,
+            bin_end_column=bin_end_column,
+            code_value_column=code_value_column,
+            ignore_instruction_column=ignore_instruction_column
+        )
+
+    def process_sample(self, sample, args):
+        """
+        Process sample
+
+        Args:
+            sample:
+            args:
+
+        Returns:
+
+        """
+
+        time_filter_delta = self.get_time_filter_delta(
+            start_time=args.eval_start_time,
+            time_gap=args.eval_time_gap
+        )
+
+        # Get processed encounter history
+        encounter_history = self.get_encounter_history_for_task(
+            patient_id=sample[args.patient_id_column],
+            current_time=sample[args.sample_result_date_column],
+            past_time_delta=None,
+            future_time_delta=time_filter_delta,
+            use_log_position=args.use_log_position,
+            time_difference_normalize=args.time_difference_normalize
+        )
+
+        eval_sample = self.get_eval_sample(
+            eval_code=args.eval_code,
+            eval_start_time=args.eval_start_time,
+            eval_end_time=args.eval_end_time
+        )
+
+        # time delta to start time of code
+        instruction_samples = self.get_instruction_samples(
+            encounter_history=encounter_history,
+            sample_bin_frequency=1.0,
+            shuffle=args.shuffle_bins
+        )
+
+        eval_instruction_samples = self.get_eval_instruction_samples(
+            instruction_samples=instruction_samples, eval_sample=eval_sample
+        )
+
+        return self.convert_samples_to_instructions(
+            instruction_samples=eval_instruction_samples,
+            ignore_time_instruction=True
+        )
+
+    def get_eval_sample(self, eval_code: str, eval_start_time: int, eval_end_time: int) -> pd.DataFrame:
+        """
+        Create a row/dataframe that contains details about the evaluation code
+
+        Args:
+            eval_code (str): The code to evaluate
+            eval_start_time (int): The start time period of the code evaluation prompt
+            eval_end_time (int): The end time period of the code evaluation prompt
+
+        Returns:
+            (pd.DataFrame): The dataframe that contains the code, start and end times
+        """
+        eval_sample = {
+            self._code_column: [eval_code],
+            self._code_value_column: [''],
+            self._bin_start_column: [eval_start_time],
+            self._bin_end_column: [eval_end_time],
+            self._ignore_instruction_column: [False]
+        }
+        return pd.DataFrame(eval_sample)
+
+    def get_ignore_instructions(self, instruction_samples: pd.DataFrame) -> Union[List[bool], pd.Series]:
+        """
+        Return a dataframe of bools that represents whether to ignore instructions in the
+        output/labels
+
+        Args:
+           instruction_samples (pd.DataFrame): The samples that will be used as instructions
+
+        Returns:
+            ignore_instruction (Optional[Sequence[bool]]): Keep these instructions in the
+            input but don't use them as labels - ignore their output while training.
+        """
+        return [True] * len(instruction_samples)
+
+    @staticmethod
+    def get_eval_instruction_samples(
+            instruction_samples: pd.DataFrame,
+            eval_sample: pd.DataFrame
+    ) -> pd.DataFrame:
+        """
+        Add the eval sample to the instruction samples and return. The returned dataframe can
+        then be used to create the prompt - which can then be passed to the model for evaluation.
+
+        Args:
+            instruction_samples (pd.DataFrame): Contains the samples/encounters from the trajectory
+            eval_sample (pd.DataFrame): The eval sample
+
+        Returns:
+            (pd.DataFrame): Dataframe that contains both the trajectory samples and the eval sample
+        """
+        return pd.concat([instruction_samples, eval_sample])
+
+    @staticmethod
+    def get_time_filter_delta(start_time: int, time_gap: int) -> str:
+        """
+        Get a time filter - this will filter out all the encounters after this time
+        We take the eval start time and subtract any time delta we want from it
+        This will be used to get all encounters until this time delta
+
+        Args:
+            start_time (int): The start time period of evaluation
+            time_gap (int): A time gap between the start time of evaluation
+            and a time delta to the rest of the encounters occurring before it
+
+        Returns:
+            (str): A time delta string that can be used by pandas
+        """
+        time_delta = start_time - time_gap
+        return f'{time_delta}d'
