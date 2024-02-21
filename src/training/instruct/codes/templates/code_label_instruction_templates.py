598fe5cf473cd58fcef91b2d992e13d8c9aef206
Prajwal Kailas <prajwal967@gmail.com>
2024-02-21 11:34:13 -0500

    remove trajectory params

---
+++ src/training/instruct/codes/templates/code_label_instruction_templates.py
@@ -0,0 +1,123 @@
+"""
+Define the diagnostic code status classification task - The task uses templates, inputs and targets.
+This class will return the instruction string. It will make use of the  instruction data
+and the instruction template to create the instruction string.
+"""
+from typing import Tuple, Union
+
+
+class CodeLabelPredictionInstructions(object):
+    """
+    Define the code trajectory prediction task. Encapsulate the templates
+    inputs, outputs to build the final instruction. The instruction is built based
+    on the trajectory of encounters.
+    """
+
+    def __init__(
+            self,
+            past_input,
+            future_input,
+            targets: str = '',
+            inputs_prefix: str = "",
+            targets_prefix: str = "",
+            x_y_delimiter: str = ":",
+            task_definition: str = '',
+            task_name: str = 'code_trajectory_prediction'
+    ):
+        """
+        Initialize the variables
+
+        Args:
+            past_input (str): The string that represents the past-time instruction input
+            future_input (str): The string that represents the future-time instruction input
+            targets (str): The string that represents the instruction target
+            inputs_prefix (str): Append this prefix to the instruction input
+            targets_prefix (str): Append this prefix to the instruction target
+            x_y_delimiter (str): Delimiter between instruction input and target
+            time code attributes to string
+            task_definition (str, defaults to): Add the task definition to the instruction
+            task_name (str, defaults to `status_classification`): A name to define this task.
+            Used to distinguish from other tasks
+        """
+        self.task_name = task_name
+        self._task_definition = task_definition
+        self._past_input = past_input
+        self._future_input = future_input
+        self._targets = targets
+        self._inputs_prefix = inputs_prefix
+        self._targets_prefix = targets_prefix
+        self._x_y_delimiter = x_y_delimiter
+
+    def get_task_definition(self) -> str:
+        """
+        Return the task definition
+
+        Returns:
+            (str): String containing the definition of the task
+        """
+        return self._task_definition
+
+    def get_instruction(self, diagnosis: str, time: int, value: Union[str, int, float]) -> Tuple[str, str]:
+        """
+        Given a positive diagnosis and the relative time of diagnosis
+        return the instruction input and instruction target containing
+        these attributes
+
+        Args:
+            diagnosis (str): A medical/billing diagnosis
+            time (int): The time of diagnosis
+            value (Union[str, int, float]): A value to represent the diagnosis in the bin
+
+        Returns:
+            (Tuple[str, str]): Tuple that contains the positive instruction input and instruction target
+        """
+        if time < 0:
+            instruction_input = self.get_past_instruction_input(diagnosis=diagnosis, time=time)
+        else:
+            instruction_input = self.get_future_instruction_input(diagnosis=diagnosis, time=time)
+        instruction_target = self.get_instruction_target(value=value)
+
+        return instruction_input, instruction_target
+
+    def get_past_instruction_input(self, diagnosis: str, time: int) -> str:
+        """
+        Add the diagnosis to the instruction input
+
+        Args:
+            diagnosis (str): A medical/billing diagnosis
+            time (int): The time of diagnosis
+
+        Returns:
+            (str): Instruction input that contains the diagnosis
+        """
+        return self._inputs_prefix + self._past_input.format(diagnosis=diagnosis, time=time) + self._x_y_delimiter
+
+    def get_future_instruction_input(self, diagnosis: str, time: int) -> str:
+        """
+        Add the diagnosis to the instruction input
+
+        Args:
+            diagnosis (str): A medical/billing diagnosis
+            time (int): The time of diagnosis
+
+        Returns:
+            (str): Instruction input that contains the diagnosis
+        """
+        return self._inputs_prefix + self._past_input.format(diagnosis=diagnosis, time=time) + self._x_y_delimiter
+
+
+    def get_instruction_target(self, value: Union[str, int, float]) -> str:
+        """
+        Return the code value - which represents some function of the code
+        in the time bin
+
+        Args:
+            value (Union[str, int, float]): A value associated with the diagnosis
+            - e.g. count of occurrences of the code in the bin
+
+        Returns:
+            (str): The instruction target with the label
+        """
+        return str(value)
+
+
--- src/training/parameters/instruct/instruct_parameters.py
+++ src/training/parameters/instruct/instruct_parameters.py
@@ -12,18 +12,7 @@ def get_instruct_arguments():
     parser.add_argument(
         "--number-of-instructions",
         type=int,
-        nargs='+',
-        default=None,
+        default=1,
         help="The number of instruction examples to use",
-    )
-    parser.add_argument(
-        "--training-type",
-        choices=["future_instruct_trajectory", "any_instruct_trajectory", "any_instruct", "trajectory"],
-        help="The type of training to perform"
-    )
-    parser.add_argument(
-        "--label-type",
-        choices=["binary", "binary_strict", "multiclass"],
-        help="The type of labels to use for instruct tasks"
     )
     return parser
