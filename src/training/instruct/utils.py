2a17f9b5627c8a62b847eaac0a40c7f56731344d
Prajwal Kailas <prajwal967@gmail.com>
2024-03-14 17:32:07 -0400

    change instruction template

--- src/training/instruct/utils.py
+++ src/training/instruct/utils.py
@@ -3,10 +3,31 @@ from .codes.templates import (
     CodeLabelPredictionInstructionTemplate,
 )
 
+# def get_code_label_prediction_instruction_template(
+#         past_input = 'Was the patient diagnosed with {diagnosis} in the past {time} months?',
+#         future_input = 'Will the patient be diagnosed with {diagnosis} in the next {time} months?',
+#         task_definition = '',
+#         x_y_delimiter = ' ',
+#         example_separator = '\n'
+# ) -> CodeLabelPredictionInstructionTemplate:
+#     """
+#     Return the object can use the given data and instruction templates
+#     and return an instruction string for the input and output
+#
+#     Returns:
+#         (CodeTrajectoryPredictionInstructions): Object to build instruction strings (inputs and targets)
+#     """
+#     return CodeLabelPredictionInstructionTemplate(
+#         past_input=past_input,
+#         future_input=future_input,
+#         x_y_delimiter=x_y_delimiter,
+#         task_definition=task_definition,
+#         example_separator=example_separator
+#     )
+
 def get_code_label_prediction_instruction_template(
-        past_input = 'Was the patient diagnosed with {diagnosis} in the past {time} months?',
-        future_input = 'Will the patient be diagnosed with {diagnosis} in the next {time} months?',
-        task_definition = '',
+        task_definition = 'Will the patient visit the hospital for the following diseases in the next {time} months?',
+        inputs = '* {diagnosis}:',
         x_y_delimiter = ' ',
         example_separator = '\n'
 ) -> CodeLabelPredictionInstructionTemplate:
@@ -18,8 +39,7 @@ def get_code_label_prediction_instruction_template(
         (CodeTrajectoryPredictionInstructions): Object to build instruction strings (inputs and targets)
     """
     return CodeLabelPredictionInstructionTemplate(
-        past_input=past_input,
-        future_input=future_input,
+        inputs=inputs,
         x_y_delimiter=x_y_delimiter,
         task_definition=task_definition,
         example_separator=example_separator
