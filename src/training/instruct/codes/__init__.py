769d0543bdc63433ff94872673fe272c7254e5b8
Prajwal Kailas <prajwal967@gmail.com>
2024-03-20 17:56:39 -0400

    remove negative hierarchy

--- src/training/instruct/codes/__init__.py
+++ src/training/instruct/codes/__init__.py
@@ -1,5 +1,6 @@
 from .code_trajectory_instruct_tasks import (
     CodeLabelPredictionTask,
+    HierarchicalCodeLabelPredictionTask,
     SingleCodeLabelPredictionTask
 )
 from .code_trajectory_instruct_tasks_eval import (
@@ -8,5 +9,6 @@ from .code_trajectory_instruct_tasks_eval import (
 __all__ = [
     "CodeLabelPredictionTask",
     "SingleCodeLabelPredictionTask",
+    "HierarchicalCodeLabelPredictionTask",
     "CodeLabelPredictionTaskEvaluation",
 ]
