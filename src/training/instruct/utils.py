e05bc1ec1436764b7316e671073a0dba236d2559
Prajwal Kailas <prajwal967@gmail.com>
2024-04-14 11:26:59 -0400

    utils for tree

--- src/training/instruct/utils.py
+++ src/training/instruct/utils.py
@@ -1,36 +1,16 @@
 """Util functions"""
 from .codes.templates import (
     CodeLabelPredictionInstructionTemplate,
+    HierarchicalCodeLabelPredictionInstructionTemplate
 )
 from .demographics.templates import PatientDemographicsTemplate
 
-# def get_code_label_prediction_instruction_template(
-#         past_input = 'Was the patient diagnosed with {diagnosis} in the past {time} months?',
-#         future_input = 'Will the patient be diagnosed with {diagnosis} in the next {time} months?',
-#         task_definition = '',
-#         x_y_delimiter = ' ',
-#         example_separator = '\n'
-# ) -> CodeLabelPredictionInstructionTemplate:
-#     """
-#     Return the object can use the given data and instruction templates
-#     and return an instruction string for the input and output
-#
-#     Returns:
-#         (CodeTrajectoryPredictionInstructions): Object to build instruction strings (inputs and targets)
-#     """
-#     return CodeLabelPredictionInstructionTemplate(
-#         past_input=past_input,
-#         future_input=future_input,
-#         x_y_delimiter=x_y_delimiter,
-#         task_definition=task_definition,
-#         example_separator=example_separator
-#     )
 
 def get_code_label_prediction_instruction_template(
-        task_definition = 'Diagnoses in the next {time} months',
-        inputs = '* {diagnosis}:',
-        x_y_delimiter = ' ',
-        example_separator = '\n'
+        task_definition='Diagnoses in the next {time} months',
+        inputs='* {diagnosis}:',
+        x_y_delimiter=' ',
+        example_separator='\n'
 ) -> CodeLabelPredictionInstructionTemplate:
     """
     Return the object can use the given data and instruction templates
@@ -46,11 +26,37 @@ def get_code_label_prediction_instruction_template(
         example_separator=example_separator
     )
 
+
+def get_hierarchical_code_label_prediction_instruction_template(
+        task_definition='Diagnoses in the next {time} months',
+        inputs='{diagnosis}',
+        inputs_prefix='* ',
+        targets_prefix=':  ',
+        x_y_delimiter=' -> ',
+        example_separator='\n'
+) -> HierarchicalCodeLabelPredictionInstructionTemplate:
+    """
+    Return the object can use the given data and instruction templates
+    and return an instruction string for the input and output
+
+    Returns:
+        (CodeTrajectoryPredictionInstructions): Object to build instruction strings (inputs and targets)
+    """
+    return HierarchicalCodeLabelPredictionInstructionTemplate(
+        inputs=inputs,
+        inputs_prefix=inputs_prefix,
+        targets_prefix=targets_prefix,
+        x_y_delimiter=x_y_delimiter,
+        task_definition=task_definition,
+        example_separator=example_separator
+    )
+
+
 def get_patient_demographics_instruction_template(
-        task_definition = 'Patient attributes',
-        inputs = '* {category}:',
-        x_y_delimiter = ' ',
-        example_separator = '\n'
+        task_definition='Patient attributes',
+        inputs='* {category}:',
+        x_y_delimiter=' ',
+        example_separator='\n'
 ) -> PatientDemographicsTemplate:
     """
     Return the object can use the given data and instruction templates
@@ -68,10 +74,10 @@ def get_patient_demographics_instruction_template(
 
 
 def get_patient_labs_instruction_template(
-        task_definition = 'Labs in the next {time} months',
-        inputs = '* {diagnosis}:',
-        x_y_delimiter = ' ',
-        example_separator = '\n'
+        task_definition='Labs in the next {time} months',
+        inputs='* {diagnosis}:',
+        x_y_delimiter=' ',
+        example_separator='\n'
 ) -> CodeLabelPredictionInstructionTemplate:
     """
     Return the object can use the given data and instruction templates
@@ -86,4 +92,3 @@ def get_patient_labs_instruction_template(
         task_definition=task_definition,
         example_separator=example_separator
     )
-
