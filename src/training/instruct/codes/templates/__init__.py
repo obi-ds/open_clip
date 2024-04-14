678b3063f10fae7075bae55c30f382cef3d8a7e3
Prajwal Kailas <prajwal967@gmail.com>
2024-04-14 10:09:29 -0400

    add hierarchy

--- src/training/instruct/codes/templates/__init__.py
+++ src/training/instruct/codes/templates/__init__.py
@@ -1,4 +1,6 @@
 from .code_label_instruction_templates import CodeLabelPredictionInstructionTemplate
+from .code_label_instruction_templates import HierarchicalCodeLabelPredictionInstructionTemplate
 __all__ = [
     "CodeLabelPredictionInstructionTemplate",
+    "HierarchicalCodeLabelPredictionInstructionTemplate"
 ]
