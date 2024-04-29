48f28c1f660a97bc57a09c04595e3c4d74c3b982
Prajwal Kailas <prajwal967@gmail.com>
2024-04-29 16:32:00 -0400

    debug prompt

--- src/training/instruct/demographics/__init__.py
+++ src/training/instruct/demographics/__init__.py
@@ -1,2 +1,3 @@
 from .demographic_instruct_tasks import DemographicPredictionTask
-__all__ = ["DemographicPredictionTask"]
+from .demographic_instruct_prompt import DemographicPredictionPrompt
+__all__ = ["DemographicPredictionTask", "DemographicPredictionPrompt"]
