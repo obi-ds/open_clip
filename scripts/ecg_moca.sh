d83a388eaabeea85b0b06aaedbf5898e8fcf3829
Prajwal Kailas <prajwal967@gmail.com>
2024-07-01 10:14:38 -0400

    train scripts for ecg

--- scripts/ecg_moca.sh
+++ scripts/ecg_moca.sh
@@ -1,8 +1,7 @@
-# Test run
-
-export CUDA_VISIBLE_DEVICES='0,1,2,3,4,5,6,7'
+# 1. Scratch - Diagnosis - Random - Fixed - Future
+export CUDA_VISIBLE_DEVICES='0,1,2,3,4,5,6'
 torchrun \
-    --nnodes=1 --nproc_per_node=8 --master_addr=localhost --master_port=2120 \
+    --nnodes=1 --nproc_per_node=7 --master_addr=localhost --master_port=2120 \
     -m main \
     --train-data="/mnt/obi0/phi/ehr_projects/bloodcell_clip/data/cardiac/mgh/mgh_train_2403/shard_{0000..0078}.tar"  \
     --val-data="/mnt/obi0/phi/ehr_projects/bloodcell_clip/data/cardiac/mgh/mgh_val_2403/shard_{0000..0010}.tar"  \
@@ -10,26 +9,72 @@ torchrun \
     --val-num-samples 35200 \
     --dataset-type icddataset \
     --workers 8 \
-    --batch-size 64 \
-    --accum-freq 8 \
-    --epochs 325 \
-    --lr 5e-5 \
+    --batch-size 100 \
+    --accum-freq 3 \
+    --epochs 250 \
+    --lr-scheduler cosine \
+    --lr 2.5e-5 \
+    --lr-cooldown-end 2.5e-6 \
     --beta1 0.9 \
-    --beta2 0.98 \
-    --eps 1e-6 \
+    --beta2 0.999 \
+    --eps 1e-8 \
     --wd 0.1 \
     --grad-clip-norm 1.0 \
-    --warmup 4000 \
-    --lr-scheduler cosine \
-    --lr-cooldown-end 5e-6 \
-    --coca-caption-loss-weight 1.0 \
-    --coca-contrastive-loss-weight 0.0 \
+    --warmup 10000 \
     --precision amp \
     --save-frequency 10 \
     --val-frequency 10 \
-    --zeroshot-frequency 0 \
-    --local-loss \
-    --gather-with-grad \
+    --loss-function lm \
+    --report-to wandb \
+    --code-column phecode \
+    --wandb-project-name="open-clip-phe-test-runs" \
+    --encounter-file="/mnt/obi0/phi/ehr_projects/bloodcell_clip/data/cardiac/all_encounters_2308_with_phecodes_with_na.parquet.check" \
+    --demographic-file=/mnt/obi0/phi/ehr_projects/bloodcell_clip/data/cardiac/demographics_2404.parquet \
+    --labs-folder=/mnt/obi0/phi/ehr_projects/bloodcell_clip/data/cardiac/labs \
+    --time-difference-normalize 1 \
+    --number-of-instructions 1 \
+    --k-shot 1 \
+    --max_seq_length 64 \
+    --pad_id 1 \
+    --add-img-token \
+    --negatives-type random \
+    --tasks diagnosis \
+    --training-eval-codes CV_424.4 EM_249 GU_627.2 NS_324.11 CV_416.214 \
+    --fixed-position-range \
+    --future-only \
+    --name ecg_moca_scratch_diagnosis_k_1_random_fixed_future_250_trial_18 \
+    --model ecg_moca_biogpt_scratch \
+    --seed 0
+
+
+
+# 2. Scratch - Labs - Demographics - Diagnosis - Random - Fixed - Future
+export CUDA_VISIBLE_DEVICES='0,1,2,3,4,5,6'
+torchrun \
+    --nnodes=1 --nproc_per_node=7 --master_addr=localhost --master_port=2120 \
+    -m main \
+    --train-data="/mnt/obi0/phi/ehr_projects/bloodcell_clip/data/cardiac/mgh/mgh_train_2403/shard_{0000..0078}.tar"  \
+    --val-data="/mnt/obi0/phi/ehr_projects/bloodcell_clip/data/cardiac/mgh/mgh_val_2403/shard_{0000..0010}.tar"  \
+    --train-num-samples 252800 \
+    --val-num-samples 35200 \
+    --dataset-type icddataset \
+    --workers 8 \
+    --batch-size 75 \
+    --accum-freq 4 \
+    --epochs 250 \
+    --lr-scheduler cosine \
+    --lr 2.5e-5 \
+    --lr-cooldown-end 2.5e-6 \
+    --beta1 0.9 \
+    --beta2 0.999 \
+    --eps 1e-8 \
+    --wd 0.1 \
+    --grad-clip-norm 1.0 \
+    --warmup 10000 \
+    --precision amp \
+    --save-frequency 10 \
+    --val-frequency 10 \
+    --loss-function lm \
     --report-to wandb \
     --code-column phecode \
     --wandb-project-name="open-clip-phe-test-runs" \
@@ -46,21 +91,21 @@ torchrun \
     --add-img-token \
     --distance-threshold 60 \
     --negatives-type random \
-    --tasks labs demographics diagnosis \
+    --tasks demographics diagnosis labs \
     --task-shuffle \
+    --training-eval-codes CV_424.4 EM_249 GU_627.2 NS_324.11 CV_416.214 \
     --fixed-position-range \
     --future-only \
-    --name test_moca_1 \
-    --model ecg_moca_biogpt \
+    --name ecg_moca_scratch_demographics_diagnosis_labs_random_fixed_future_250_trial_7 \
+    --model ecg_moca_biogpt_scratch \
     --seed 0
 
 
 
-# TEST
-
-export CUDA_VISIBLE_DEVICES='0,1'
+# 3. Scratch - Diagnosis - Random - Fixed - Future - Frozen
+export CUDA_VISIBLE_DEVICES='0,1,2,3,4,5,6'
 torchrun \
-    --nnodes=1 --nproc_per_node=2 --master_addr=localhost --master_port=2120 \
+    --nnodes=1 --nproc_per_node=7 --master_addr=localhost --master_port=2120 \
     -m main \
     --train-data="/mnt/obi0/phi/ehr_projects/bloodcell_clip/data/cardiac/mgh/mgh_train_2403/shard_{0000..0078}.tar"  \
     --val-data="/mnt/obi0/phi/ehr_projects/bloodcell_clip/data/cardiac/mgh/mgh_val_2403/shard_{0000..0010}.tar"  \
@@ -68,26 +113,73 @@ torchrun \
     --val-num-samples 35200 \
     --dataset-type icddataset \
     --workers 8 \
-    --batch-size 16 \
-    --accum-freq 1 \
-    --epochs 325 \
-    --lr 1e-6 \
+    --batch-size 100 \
+    --accum-freq 3 \
+    --epochs 250 \
+    --lr-scheduler cosine \
+    --lr 2.5e-5 \
+    --lr-cooldown-end 2.5e-6 \
     --beta1 0.9 \
-    --beta2 0.98 \
-    --eps 1e-6 \
+    --beta2 0.999 \
+    --eps 1e-8 \
     --wd 0.1 \
     --grad-clip-norm 1.0 \
-    --warmup 4000 \
-    --lr-scheduler cosine \
-    --lr-cooldown-end 5e-6 \
-    --coca-caption-loss-weight 1.0 \
-    --coca-contrastive-loss-weight 0.0 \
+    --warmup 10000 \
     --precision amp \
-    --save-frequency 1 \
-    --val-frequency 1 \
-    --zeroshot-frequency 0 \
-    --local-loss \
-    --gather-with-grad \
+    --save-frequency 10 \
+    --val-frequency 10 \
+    --loss-function lm \
+    --report-to wandb \
+    --code-column phecode \
+    --wandb-project-name="open-clip-phe-test-runs" \
+    --encounter-file="/mnt/obi0/phi/ehr_projects/bloodcell_clip/data/cardiac/all_encounters_2308_with_phecodes_with_na.parquet.check" \
+    --demographic-file=/mnt/obi0/phi/ehr_projects/bloodcell_clip/data/cardiac/demographics_2404.parquet \
+    --labs-folder=/mnt/obi0/phi/ehr_projects/bloodcell_clip/data/cardiac/labs \
+    --time-difference-normalize 1 \
+    --number-of-instructions 1 \
+    --k-shot 1 \
+    --max_seq_length 64 \
+    --pad_id 1 \
+    --add-img-token \
+    --negatives-type random \
+    --tasks diagnosis \
+    --training-eval-codes CV_424.4 EM_249 GU_627.2 NS_324.11 CV_416.214 \
+    --fixed-position-range \
+    --future-only \
+    --name ecg_moca_pretrained_frozen_diagnosis_k_1_random_fixed_future_250_trial_5 \
+    --model ecg_moca_biogpt \
+    --lock-text \
+    --seed 0
+
+
+
+# 4. Scratch - Labs - Demographics - Diagnosis - Random - Fixed - Future - Frozen
+export CUDA_VISIBLE_DEVICES='0,1,2,3,4,5,6'
+torchrun \
+    --nnodes=1 --nproc_per_node=7 --master_addr=localhost --master_port=2120 \
+    -m main \
+    --train-data="/mnt/obi0/phi/ehr_projects/bloodcell_clip/data/cardiac/mgh/mgh_train_2403/shard_{0000..0078}.tar"  \
+    --val-data="/mnt/obi0/phi/ehr_projects/bloodcell_clip/data/cardiac/mgh/mgh_val_2403/shard_{0000..0010}.tar"  \
+    --train-num-samples 252800 \
+    --val-num-samples 35200 \
+    --dataset-type icddataset \
+    --workers 8 \
+    --batch-size 75 \
+    --accum-freq 4 \
+    --epochs 250 \
+    --lr-scheduler cosine \
+    --lr 2.5e-5 \
+    --lr-cooldown-end 2.5e-6 \
+    --beta1 0.9 \
+    --beta2 0.999 \
+    --eps 1e-8 \
+    --wd 0.1 \
+    --grad-clip-norm 1.0 \
+    --warmup 10000 \
+    --precision amp \
+    --save-frequency 10 \
+    --val-frequency 10 \
+    --loss-function lm \
     --report-to wandb \
     --code-column phecode \
     --wandb-project-name="open-clip-phe-test-runs" \
@@ -99,16 +191,17 @@ torchrun \
     --k-shot 1 1 1 1 1 2 2 2 3 4 \
     --k-shot-demographics 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 2 2 2 3 4 \
     --k-shot-labs 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 2 2 2 3 4 \
-    --training-eval-codes CV_424.4 \
-    --max_seq_length 64 \
+    --max_seq_length 128 \
     --pad_id 1 \
     --add-img-token \
     --distance-threshold 60 \
     --negatives-type random \
-    --tasks diagnosis \
+    --tasks demographics diagnosis labs \
     --task-shuffle \
+    --training-eval-codes CV_424.4 EM_249 GU_627.2 NS_324.11 CV_416.214 \
     --fixed-position-range \
     --future-only \
-    --name test_moca_32 \
+    --name ecg_moca_pretrained_frozen_demographics_diagnosis_labs_random_fixed_future_250_trial_1 \
     --model ecg_moca_biogpt \
+    --lock-text \
     --seed 0
